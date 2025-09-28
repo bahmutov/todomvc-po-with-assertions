155c96aa91329e260fd11e945be06fa52b689342
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-09-28 15:28:33 -0400

    remove custom command

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,2 @@
 import 'cypress-map'
 import 'cypress-plugin-steps'
-
-// define a simple custom command to add a todo via UI
-Cypress.Commands.add('addTodo', (text: string) => {
-  cy.get('.new-todo').type(text + '{enter}')
-  // check when the new todo appears in the list
-  cy.contains('.todo-list li', text)
-})
